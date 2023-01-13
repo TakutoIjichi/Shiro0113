8b9f70ded835d3e619b6815a98786a72864a3bc6
伊知地拓斗 <ijichitakuto@ichichitakutonoMacBook-Pro.local>
2023-01-13 16:16:05 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
 'use strict'
 
 {
+  
+  
+  $(function() {
   const open = document.getElementById('open');
   const overlay = document.querySelector('.overlay');
-  const close = document.getElementById('close');
+  $('.sp-menu').click(function() {
+    $('.bar1, .bar2, .bar3').toggleClass('open');
+    $('.overlay').toggleClass('show');
+  })
+});
 
-  open.addEventListener('click', () => {
-    overlay.classList.add('show');
-    open.classList.add('hide');
-  });
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 40) {
+    $('#header').addClass('position_change');
+  } else {
+    $('#header').removeClass('position_change');
+  }
+});
 
-  close.addEventListener('click', () => {
-    overlay.classList.remove('show');
-    open.classList.remove('hide');
-  });
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 610) {
+    $('#header').addClass('color_change');
+  } else {
+    $('#header').removeClass('color_change');
+  }
+});
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 610) {
+    $('span').addClass('bar-color');
+  } else {
+    $('span').removeClass('bar-color');
+  }
+});
+
+const overlay = document.querySelector('.overlay')
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('show');
+})
+
 }
